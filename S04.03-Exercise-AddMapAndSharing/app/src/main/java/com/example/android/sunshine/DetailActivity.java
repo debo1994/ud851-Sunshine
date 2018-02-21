9a5d72b8d00a1fd2b42f8604f6af95ac4a502521
Debajyoti Basak <d.basak.db@gmail.com>
2018-02-21 09:26:53 +0530

    S04.03-Exercise-AddMapAndSharing Complete

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,13 +1,20 @@
 package com.example.android.sunshine;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private static final String TAG = DetailActivity.class.getSimpleName();
 
     private String mForecast;
     private TextView mWeatherDisplay;
@@ -27,6 +34,54 @@ public class DetailActivity extends AppCompatActivity {
                 mWeatherDisplay.setText(mForecast);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+        inflater.inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(createShareForecastIntent());
+        return true;
+        /* Return true so that the menu is displayed in the Toolbar */
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText(mForecast + FORECAST_SHARE_HASHTAG)
+                .getIntent();
+        return shareIntent;
+    }
+
+    private void openLocationinMap() {
+        String addressString = "1600 Ampitheatre Parkway, CA";
+        Uri geoLocation = Uri.parse("geo:0,0?q=" + addressString);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(TAG, "Couldn't call " + geoLocation.toString() + ", no receiving apps installed!");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            openLocationinMap();
+            return true;
+        }
+
+        // TODO (2) Launch the map when the map menu item is clicked
+
+        return super.onOptionsItemSelected(item);
     }
 
     // TODO (3) Create a menu with an item with id of action_share
